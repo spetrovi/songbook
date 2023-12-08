9f35cc4673c24493356b57685e8756f3a43d9e62
Samuel Petrovic <samuel.petrovic@centrum.sk>
2023-12-08 13:19:38 +0100

    Adding pydantic support for our custom Source type
    Signed-off-by: Samuel Petrovic <samuel.petrovic@centrum.sk>

--- app/songs/models.py
+++ app/songs/models.py
@@ -2,12 +2,23 @@ import uuid
 from sqlmodel import Field, SQLModel, Relationship, Enum
 from typing import Optional
 from sqlalchemy import Column, LargeBinary
+from typing import Any
+
+from pydantic_core import CoreSchema, core_schema
+
+from pydantic import GetCoreSchemaHandler
 
 
 class SourceType(str, Enum):
     book = "book"
     cd = "cd"
     lp = "lp"
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.no_info_after_validator_function(cls, handler(str))
 
 
 class Person(SQLModel, table=True):
