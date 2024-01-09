bfa580482c281c28ce508ef54635bf55572ef7a7
spetrovi <spetrovi@redhat.com>
2024-01-09 15:15:01 +0100

    Admin login check
    Also manage.py for createsuperuser
    
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- app/users/decorators.py
+++ app/users/decorators.py
@@ -1,8 +1,13 @@
 from functools import wraps
 
 from fastapi import Request
+from sqlalchemy.exc import NoResultFound
+from sqlmodel import select
 
+from .exceptions import AdminRequiredException
 from .exceptions import LoginRequiredException
+from .models import User
+from app.db import get_session
 
 
 def login_required(func):
@@ -20,8 +25,16 @@ def admin_login_required(func):
     def wrapper(request: Request, *args, **kwargs):
         if not request.user.is_authenticated:
             raise LoginRequiredException(status_code=401)
-        # if not request.user.is_admin:
-        #    raise LoginRequiredException(stat
+        with get_session() as session:
+            statement = (
+                select(User)
+                .where(User.user_id == request.user.username)
+                .where(User.is_admin)
+            )
+            try:
+                session.exec(statement).one()
+            except NoResultFound:
+                raise AdminRequiredException(status_code=401)
         return func(request, *args, **kwargs)
 
     return wrapper
---
+++ manage.py
@@ -0,0 +1,34 @@
+import argparse
+from getpass import getpass
+
+from app.users.models import User
+
+
+def create_superuser():
+    """
+    Create a superuser in the database
+    """
+    email = input("Enter superuser email: ")
+    password = getpass("Enter superuser password: ")
+
+    try:
+        User.create_user(email, password, is_admin=True)
+        print(f"Superuser {email} created successfully.")
+    except Exception as e:
+        print(f"Error creating superuser: {e}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple management for database")
+
+    parser.add_argument(
+        "--createsuperuser", action="store_true", help="Create a superuser"
+    )
+    args = parser.parse_args()
+
+    if args.createsuperuser:
+        create_superuser()
+
+
+if __name__ == "__main__":
+    main()
