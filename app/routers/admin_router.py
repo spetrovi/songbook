2da89ff6d6121327c8cd0d6ae250186cae8571c4
spetrovi <spetrovi@redhat.com>
2024-01-30 13:41:49 +0100

    Db rework
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- app/routers/admin_router.py
+++ app/routers/admin_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Request
 from fastapi.responses import HTMLResponse
-from sqlmodel import select
+from sqlalchemy import inspect
+from sqlmodel import Session
 
 from app import db
 from app.shortcuts import render
@@ -22,45 +24,40 @@ def admin_view(request: Request):
 
 @router.get("/{object}", response_class=HTMLResponse)
 @admin_login_required
-def admin_users_view(request: Request, object: str):
+def admin_users_view(
+    request: Request, object: str, session: Session = Depends(db.yield_session)
+):
     cls = None
-    # TODO do some clever mapping here
     if object == "users":
         cls = User
-        id_name = "user_id"
-        display_fields = ["email", "is_admin"]
     if object == "songbooks":
         cls = Songbook
-        id_name = "songbook_id"
-        display_fields = ["user_id", "songbook_id"]
     if object == "entries":
         cls = Entry
-        id_name = "id"
-        display_fields = ["songbook_id", "song_id", "order"]
 
-    with db.get_session() as session:
-        objects = session.exec(select(cls)).all()
-        context_objects = [item.dict() for item in objects]
-        for item in context_objects:
-            item["id"] = item[id_name]
-            item["cls_name"] = cls.__name__
-            item["display_fields"] = display_fields
+    instances = session.query(cls).all()
+
+    id_name = (inspect(cls).primary_key)[0].name
+
+    columns = [column.name for column in inspect(cls).c]
     return render(
         request,
         "admin/objects.html",
-        {"context_objects": context_objects},
+        {"instances": instances, "columns": columns, "id_name": id_name},
         status_code=200,
     )
 
 
 @router.delete("/delete/{object}/{id}", response_class=HTMLResponse)
 @admin_login_required
-def admin_delete_object(request: Request, object: str, id: str):
+def admin_delete_object(
+    request: Request, object: str, id: str, session: Session = Depends(db.yield_session)
+):
     # TODO do some clever mapping here
     if object == "User":
-        User.delete_user(id)
+        User.delete_user(id, session)
     if object == "Songbook":
-        Songbook.delete_songbook(id)
+        Songbook.delete_songbook(id, session)
     if object == "Entry":
-        Entry.delete_entry(id)
+        Entry.delete_entry(id, session)
     return HTMLResponse("", status_code=200)
--- app/routers/htmx_router.py
+++ app/routers/htmx_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Form
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from sqlmodel import select
+from sqlmodel import Session
 
 from app import db
 from app.shortcuts import render
@@ -18,8 +20,13 @@ router = APIRouter()
 @router.post(
     "/add_song_to_songbook/{songbook_id}/{song_id}", response_class=HTMLResponse
 )
-def add_song_to_songbook(request: Request, songbook_id: str, song_id: str):
-    Entry.add_song(songbook_id, song_id)
+def add_song_to_songbook(
+    request: Request,
+    songbook_id: str,
+    song_id: str,
+    session: Session = Depends(db.yield_session),
+):
+    Entry.add_song(songbook_id, song_id, session)
     return HTMLResponse("", status_code=200)
 
 
@@ -27,17 +34,24 @@ def add_song_to_songbook(request: Request, songbook_id: str, song_id: str):
 @router.delete(
     "/remove_song_from_songbook/{songbook_id}/{song_id}", response_class=HTMLResponse
 )
-def remove_song_from_songbook(request: Request, songbook_id: str, song_id: str):
-    Entry.remove_song(songbook_id, song_id)
+def remove_song_from_songbook(
+    request: Request,
+    songbook_id: str,
+    song_id: str,
+    session: Session = Depends(db.yield_session),
+):
+    Entry.remove_song(songbook_id, song_id, session)
     return HTMLResponse("", status_code=200)
 
 
 @router.post("/songbook/sort_form", response_class=HTMLResponse)
-async def post_songbook_sortform(request: Request):
+async def post_songbook_sortform(
+    request: Request, session: Session = Depends(db.yield_session)
+):
     form_data = await request.form()
     songbook_id = form_data.get("songbook_id")
     item_list = list(form_data.items())
-    songs = Entry.reorder_songs(item_list, songbook_id)
+    songs = Entry.reorder_songs(item_list, songbook_id, session)
 
     return render(
         request,
@@ -48,58 +62,63 @@ async def post_songbook_sortform(request: Request):
 
 @login_required
 @router.delete("/delete_songbook/{songbook_id}", response_class=HTMLResponse)
-def delete_songbook(request: Request, songbook_id: str):
-    Songbook.delete_songbook(request.user.username, songbook_id)
+def delete_songbook(
+    request: Request, songbook_id: str, session: Session = Depends(db.yield_session)
+):
+    Songbook.delete_songbook(request.user.username, songbook_id, session)
     return HTMLResponse("", status_code=200)
 
 
 @login_required
 @router.get("/create_songbook", response_class=HTMLResponse)
-def create_songbook(request: Request):
-    with db.get_session() as session:
-        if not session.exec(
-            select(User).where(User.user_id == request.user.username)
-        ).one():
-            raise Exception("User doesn't exists")
-        songbook = Songbook(user_id=request.user.username)
-        session.add(songbook)
-        session.commit()
-        return render(
-            request,
-            "snippets/new_songbook_slide.html",
-            {"songbook": songbook},
-        )
+def create_songbook(request: Request, session: Session = Depends(db.yield_session)):
+    if not session.exec(
+        select(User).where(User.user_id == request.user.username)
+    ).one():
+        raise Exception("User doesn't exists")
+    songbook = Songbook.create_songbook(request.user.username, session)
+    return render(
+        request,
+        "snippets/new_songbook_slide.html",
+        {"songbook": songbook},
+    )
 
 
 @login_required
 @router.put("/rename_songbook/{songbook_id}", response_class=HTMLResponse)
-def rename_songbook(request: Request, songbook_id: str, title: str = Form(...)):
-    with db.get_session() as session:
-        statement = (
-            select(Songbook)
-            .where(Songbook.user_id == request.user.username)
-            .where(Songbook.songbook_id == songbook_id)
-        )
-        songbook = session.exec(statement).one()
-        songbook.title = title
-        session.commit()
-        return render(request, "snippets/songbook_body.html", {"songbook": songbook})
+def rename_songbook(
+    request: Request,
+    songbook_id: str,
+    title: str = Form(...),
+    session: Session = Depends(db.yield_session),
+):
+    statement = (
+        select(Songbook)
+        .where(Songbook.user_id == request.user.username)
+        .where(Songbook.songbook_id == songbook_id)
+    )
+    songbook = session.exec(statement).one()
+    songbook.title = title
+    session.commit()
+    session.refresh(songbook)
+    return render(request, "snippets/songbook_body.html", {"songbook": songbook})
 
 
 @router.get("/rename_songbook/{songbook_id}", response_class=HTMLResponse)
-def get_rename_songbook(request: Request, songbook_id: str):
-    with db.get_session() as session:
-        statement = (
-            select(Songbook)
-            .where(Songbook.user_id == request.user.username)
-            .where(Songbook.songbook_id == songbook_id)
-        )
-        songbook = session.exec(statement).one()
-        return render(
-            request,
-            "snippets/rename_songbook_form.html",
-            {"songbook": songbook},
-        )
+def get_rename_songbook(
+    request: Request, songbook_id: str, session: Session = Depends(db.yield_session)
+):
+    statement = (
+        select(Songbook)
+        .where(Songbook.user_id == request.user.username)
+        .where(Songbook.songbook_id == songbook_id)
+    )
+    songbook = session.exec(statement).one()
+    return render(
+        request,
+        "snippets/rename_songbook_form.html",
+        {"songbook": songbook},
+    )
 
 
 @login_required
