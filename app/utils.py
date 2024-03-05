223a77ec4faf75be1b2277cfd27fb7a958514cca
spetrovi <spetrovi@redhat.com>
2024-03-05 11:12:06 +0100

    Generate dark-theme pngs
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,8 @@ import json
 import subprocess
 from pathlib import Path
 
+from PIL import Image
+from PIL import ImageOps
 from pydantic import BaseModel
 from pydantic.error_wrappers import ValidationError
 
@@ -26,6 +28,35 @@ def valid_schema_data_or_error(raw_data: dict, SchemaModel: BaseModel):
     return data, errors
 
 
+def dark_theme_png(dest_path):
+    # Open the inverted PNG image
+    image = Image.open(dest_path / "source.cropped.png")
+
+    image = ImageOps.invert(image)
+
+    # Define the target colors (dark gray and slightly darker gray)
+    dark_gray_color = (33, 37, 41)  # RGB values for #212529
+    darker_white_color = (200, 200, 200)  # Adjusted slightly darker shade of white
+
+    # Get the image dimensions
+    width, height = image.size
+
+    # Loop through each pixel in the image
+    for x in range(width):
+        for y in range(height):
+            pixel_color = image.getpixel((x, y))
+
+            # If the pixel color is black, replace it with the dark gray color
+            if pixel_color == (0, 0, 0):
+                image.putpixel((x, y), dark_gray_color)
+            # If the pixel color is white, replace it with the slightly darker white color
+            elif pixel_color == (255, 255, 255):
+                image.putpixel((x, y), darker_white_color)
+
+    # Save the modified image
+    image.save(dest_path / "source.cropped.dark.png")
+
+
 def build_song(song):
     dest_path = Path("app/tmp/" + str(song.id))
     pdf_path = dest_path / "source.pdf"
@@ -39,6 +70,8 @@ def build_song(song):
         file.write(source_lytex)
 
     subprocess.run(["lilypond", "-o", dest_path.resolve(), source.resolve()])
+
+    dark_theme_png(dest_path)
     return
 
 
